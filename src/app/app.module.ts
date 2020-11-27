6aca49bfc726c1ece76066951759dcc80f3c6cae
itai natan <itai.natan@herolo.co.il>
2020-11-27 11:41:55 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {ScrollingModule} from '@angular/cdk/scrolling';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
