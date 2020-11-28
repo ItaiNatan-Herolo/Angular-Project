39a1b6838adfb746713e4be68da222f1575efda2
itai natan <itai.natan@herolo.co.il>
2020-11-29 00:57:40 +0200

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 // Components
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { TableComponent } from './components/table/table.component';
     BrowserModule,
     BrowserAnimationsModule,
     ScrollingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
